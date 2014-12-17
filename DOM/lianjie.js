6e09b295740267e289aff1efd76a11b3196205a6
DLEif <181713574@qq.com>
2014-12-17 22:55:04 +0800

    pass form

--- DOM/lianjie.js
+++ DOM/lianjie.js
@@ -4,104 +4,104 @@ function checkform(){
   judgeNull();
 
   var totalInput = getInput1("fullName", "统一建模语言") + getInput2("property1", corret) +
-      getInput2("property2", corret) + getInput2("property3", corret);
+    getInput2("property2", corret) + getInput2("property3", corret);
 
-  var totalSelete = getSelete("relation0", "B") + getSelete("sustain0", "C");
+  var totalSelete = getSelete("abc", "B") + getSelete("cba", "C");
 
-  var totalMoreSelete = getMoreSelete("sustain0", "A,B,D") + getDuoSelete("classify0", "A,B,C");
+  var totalMoreSelete = getMoreSelete("classify", "A,B,D") + getMoreSelete("comprise", "A,B,C");
 
   var totalJudge = getJudge("judge1", "false") + getJudge("judge2", "true");
 
   var totalText = getText('talk');
 
-  score = totalSelete + totalMoreSelete + totalInput + totalText + totalJudge;
+  score = totalSelete + totalInput + totalMoreSelete + totalText + totalJudge;
 
-  document.getElementById("print").innerHTML = score;
+  document.getElementById("print").value = score;
 
 }
 
 function judgeNull(){
-  var personName = document.getElementById("name");
-  var personId = document.getElementById("id");
-  var personClass = document.getElementById("class");
+  var personName = document.getElementById("name").value;
+  var personId = document.getElementById("id").value;
+  var personClass = document.getElementById("class").value;
   if(personName === ''|| personId === '' || personClass === ''){
     alert("Please improve personal information.");
   }
 }
 
 function getInput1(input, anwser) {
-  var total = 0;
+  var score = 0;
   var inputAnwser = document.getElementById(input).value;
 
   if (inputAnwser === anwser) {
-    total = 15;
+    score += 15;
   }
-
-  return total;
+  return score;
 }
 
 function getInput2(input, corret) {
-  var total = 0;
+  var score = 0;
   var inputAnwser = document.getElementById(input).value;
 
   for (var i = 0; i < corret.length; i++) {
-    if (inputAnwser === corret[i]) {
-      total += 5;
+    if (inputAnwser === corret[i] ) {
+      score += 5;
     }
   }
-  return total;
+  return score;
 }
 
 function getSelete(selete, anwser) {
-  var total = 0;
-  var radio = document.getElementsById(selete);
+  var score = 0;
+  var radio = document.getElementsByName(selete);
 
   for (var i = 0; i < radio.length; i++) {
     if (radio[i].checked === true && radio[i].value === anwser) {
-      total = 5;
+      score = 5;
     }
   }
-  return total;
+  return score;
 }
 
-function getDuoSelete(moreSelete, anwser) {
+function getMoreSelete(moreSelete, anwser) {
   var array = [];
-  var total = 0;
-  var moreSeleteAnwser = document.getElementsById(moreSelete);
+  var score = 0;
+  var moreSeleteAnwser = document.getElementsByName(moreSelete);
 
-  for (var i = 0; i < duoSeleteAnwser.length; i++) {
-    if (duoSeleteAnwser[i].checked === true) {
+  for (var i = 0; i < moreSeleteAnwser.length; i++) {
+    if (moreSeleteAnwser[i].checked === true) {
       array.push(moreSeleteAnwser[i].value);
     }
   }
-
+  alert(array.toString());
   if (array.toString() === anwser) {
-    total = 10;
+    score += 10;
   }
 
-  return total;
+  return score;
 }
 
 function getJudge(judge, anwser) {
-  var total = 0;
+  var score = 0;
   var judgeAnwser = document.getElementsByName(judge);
 
   for (var i = 0; i < judgeAnwser.length; i++) {
     if (judgeAnwser[i].checked === true && judgeAnwser[i].value === anwser) {
-      total = 10;
+      score = 10;
     }
   }
-  return total;
+  return score;
 }
 
 function getText() {
-  var total = 0;
-  var input = document.getElementById("").value;
+  var score = 0;
+  var input = document.getElementById("talk").value;
 
-  if (input === "模型是对现实世界的简化和抽象,模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体;可以是某种图形;或者是一种数学表达式。") {
-    total = 20;
+  if (input === "模型是对现实世界的简化和抽象,模型是对所研究的系统、过程、事物或概念的一种" +
+                "表达形式。可以是物理实体;可以是某种图形;或者是一种数学表达式。") {
+    score = 20;
   }
-  return total;
+  return score;
 }
 
 // var score = 0;
